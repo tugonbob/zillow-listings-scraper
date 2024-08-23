7771825a0a6e1010af9f0e33caf2ea8ef5cdce7a
tugonbob <57576522+tugonbob@users.noreply.github.com>
2024-08-23 11:48:30 -0500

    Added loop through listing type

--- scrape.py
+++ scrape.py
@@ -14,7 +14,8 @@ def generate_date_range(date_from, days=1):
 
 
 def random_sleep():
-    time_to_sleep = random.uniform(0, 10)
+    # used to avoid bot detection on consecutive api call with same proxy
+    time_to_sleep = random.uniform(0, 5)
     print(f"Sleeping for {round(time_to_sleep, 1)}")
     time.sleep(time_to_sleep)
 
@@ -38,72 +39,87 @@ if __name__ == "__main__":
     proxyManager = ProxyManager()
     cityManager = CityManager()
     cities = cityManager.get_cities_list()
-    args = {"days_to_scrape": 10}
+    args = {"days_to_scrape": 10, "listing_type": "sold"}
+    listing_type = args["listing_type"]
 
-    try:
-        for city in cities:
-            city_name = city["city"] + ", " + city["state_id"]
-            filename = f"scraped_data/{city_name}_sold.csv"
-            end_day = datetime.now()
-            start_day = end_day - timedelta(days=args["days_to_scrape"])
-            period = adjust_period_to_city_density(city["density"])
+    for city in cities:
+        # create output path
+        city_name = city["city"] + ", " + city["state_id"]
+        out_name = city["city"] + "_" + city["state_id"] + ".csv"
+        out_path = f"scraped_data/{listing_type}/{out_name}"
 
-            date = start_day
-            properties = None
-            last_working_proxy = None
+        # define start and stop date
+        end_day = datetime.now()
+        start_day = end_day - timedelta(days=args["days_to_scrape"])
 
-            while date < end_day:
-                date_from, date_to = generate_date_range(date, days=period)
-                print(city_name + ": " + date_from + " " + date_to)
+        # define how many days should be included in each scrape call
+        period = adjust_period_to_city_density(city["density"])
 
-                # Proxies to try: first the last working one, then the others
-                proxies_to_try = (
-                    [last_working_proxy] + proxyManager.valid_proxies
-                    if last_working_proxy
-                    else proxyManager.valid_proxies
-                )
+        # variables to remeber during loop
+        date = start_day  # keep track of what date the loop is currently on
+        properties = None  # used to track if all proxies failed. This will stay non if all proxies fail. Otherwise, it holds the scraped data
+        last_working_proxy = None  # used to remember the last successful proxy to be called immeadiately in the next iter of loop
 
-                for proxy in proxies_to_try:
-                    if proxy is None:
-                        continue
+        # loop through dates
+        while date < end_day:
+            # generate date range used for scrape call
+            date_from, date_to = generate_date_range(date, days=period)
+            print("Finding", listing_type, "in", city_name + ":", date_from, date_to)
 
-                    try:
-                        print(f"Trying {proxy}")
-                        properties = scrape_property(
-                            location="Houston, TX",
-                            listing_type="sold",  # or (for_sale, for_rent, pending)
-                            date_from=date_from,  # alternative to past_days
-                            date_to=date_to,
-                            extra_property_data=True,
-                            proxy=proxy,
-                            radius=100,
+            # Proxies to try: first the last working one, then the others
+            proxies_to_try = (
+                [last_working_proxy] + proxyManager.valid_proxies
+                if last_working_proxy
+                else proxyManager.valid_proxies
+            )
+
+            # loop through proxies
+            for proxy in proxies_to_try:
+                # first proxy in list will be None at init
+                if proxy is None:
+                    continue
+
+                try:
+                    # scrape real estate data with proxy
+                    print(f"Trying {proxy}")
+                    properties = scrape_property(
+                        location=city_name,
+                        listing_type=listing_type,  # or (for_sale, for_rent, pending)
+                        date_from=date_from,  # alternative to past_days
+                        date_to=date_to,
+                        extra_property_data=True,
+                        proxy=proxy,
+                        radius=100,
+                    )
+
+                    # check if scrape was successful
+                    if properties is not None:
+                        print(
+                            f"Number of properties scraped: {len(properties)} for {date_from} to {date_to}"
                         )
 
-                        if properties is not None:
-                            print(
-                                f"Number of properties scraped: {len(properties)} for {date_from} to {date_to}"
-                            )
-                            last_working_proxy = proxy
-                            break
-                    except KeyboardInterrupt:
-                        sys.exit()
-                    except:
-                        continue
+                        # save successful proxy for next scrape call
+                        last_working_proxy = proxy
 
-                # refresh proxy list if none of the proxies work
-                if properties is None:
-                    proxyManager.refresh_valid_proxies()
+                        break
+                except KeyboardInterrupt:
+                    sys.exit()
+                except:
+                    # if scrape failed, try next proxy
+                    continue
 
-                properties.to_csv(
-                    filename,
-                    mode="a",
-                    index=False,
-                    header=not os.path.exists(filename),
-                )
+            # refresh proxy list if none of the proxies work
+            if properties is None:
+                proxyManager.refresh_valid_proxies()
 
-                date += timedelta(days=period + 1)
-                random_sleep()  # maybe to avoid bot protection
+            # save scraped data, append to path as csv
+            properties.to_csv(
+                out_path,
+                mode="a",
+                index=False,
+                header=not os.path.exists(out_path),
+            )
 
-    except KeyboardInterrupt:
-        print("\nProgram interrupted by user. Exiting...")
-        sys.exit()
+            # move current date by period + 1 days. +1 is to avoid overlap of days
+            date += timedelta(days=period + 1)
+            random_sleep()  # maybe to avoid bot protection
